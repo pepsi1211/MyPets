a3506b25ff2caa3cb40280b7d8f82cb42eb5cfdc
pepsi1211 <715803334@qq.com>
2020-01-03 12:01:58 +0800

    完成主页静态页面

--- MyPets/pages/index/index.vue
+++ MyPets/pages/index/index.vue
@@ -33,7 +33,6 @@
 		<!-- 添加爱宠部分 -->
 		<view class="add">
 			<i class="add-img"></i>
-			<!-- <image src="../../static/icon/home_add_bg_img.png"></image> -->
 		</view>
 		<!-- 养宠知识 -->
 		<view class="knowledge">
@@ -54,6 +53,49 @@
 
 			</scroll-view>
 		</view>
+		<!-- 铲屎官热议部分 -->
+		<view class="talk">
+			<view class="top">
+				<text class="title">铲屎官热议</text>
+				<text class="more">更多</text>
+			</view>
+			<view class="talk-down">
+				<block v-for="(key,index) of talk_list" :key="index">
+					<view class="talk-topic">
+						<view class="talk-ask">
+							<i class="img ask"></i>
+							<text>{{key.title}}</text>
+						</view>
+						<view class="talk-reply">
+							<i class="img reply"></i>
+							<text>{{key.reply}}</text>
+						</view>
+						<view class="talk-more">
+							<text>查看全部{{key.count}}回答</text>
+							<i class="next"></i>
+						</view>
+					</view>
+				</block>
+			</view>
+		</view>
+		<!-- 它嗅故事部分 -->
+		<view class="story">
+			<view class="top">
+				<text class="title">它嗅故事</text>
+				<text class="more">更多</text>
+			</view>
+			<view class="story-down">
+				<block v-for="key in 4" :key="key">
+					<view class="story-article">
+						<image class="story-img" src="../../static/icon/story_img.png"></image>
+						<view class="story-content">
+							<text class="title">搭建用户桥梁, 倾听宠物心声</text>
+							<text class="content">家中从小养过几条小狗,至今还记得陪伴我时间最久的那只名叫灰灰,大概是因为他太可爱了</text>
+						</view>
+					</view>
+				</block>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -67,16 +109,32 @@
 						like: 0
 					},
 					{
-						title: "狗狗打架怎么阻止?",
-						watched: 52,
+						title: "狗狗为什么爱吃屎?",
+						watched: 66,
 						like: 0
 					},
 					{
-						title: "狗狗打架怎么阻止?",
-						watched: 52,
+						title: "狗狗可以吃葡萄吗?",
+						watched: 79,
 						like: 0
 					},
 				],
+				talk_list: [{
+						title: "被猫抓了 渗出一点点血 不用打狂犬疫苗吧",
+						reply: "家养猫没有经常出去的话,保守一点可以用十日观察法,一般猫狗患狂犬病,十日内基本挂了",
+						count: 1
+					},
+					{
+						title: "被猫抓了 渗出一点点血 不用打狂犬疫苗吧",
+						reply: "家养猫没有经常出去的话,保守一点可以用十日观察法,一般猫狗患狂犬病,十日内基本挂了",
+						count: 2
+					},
+					{
+						title: "被猫抓了 渗出一点点血 不用打狂犬疫苗吧",
+						reply: "家养猫没有经常出去的话,保守一点可以用十日观察法,一般猫狗患狂犬病,十日内基本挂了",
+						count: 1
+					},
+				]
 			}
 		},
 		onLoad() {
@@ -173,9 +231,12 @@
 	}
 
 	// 养宠知识部分
-	.knowledge {
+	.knowledge,
+	.talk,
+	.story {
 		background: #fff;
 		width: 100%;
+		margin-bottom: 20upx;
 
 		.top {
 			border-bottom: 1px solid #f4f4f4;
@@ -191,22 +252,24 @@
 			.more {
 				font-size: 12px;
 				color: #c6c6c6;
+				vertical-align: top;
 
 				&:after {
 					content: "";
 					display: inline-block;
-					width: 24upx;
-					height: 24upx;
-					margin: 4upx 0 0 4upx;
+					width: 20upx;
+					height: 20upx;
+					margin-left: 4upx;
 					background: url(../../static/icon/arrow_right.png) no-repeat;
 					background-size: contain;
 				}
 			}
 		}
 
-		.bottom { 
+		.bottom {
 			height: 330upx;
 			white-space: nowrap;
+
 			// line-height: 330upx;
 			.knowledge-bg {
 				display: inline-block;
@@ -216,16 +279,138 @@
 				background: url(../../static/icon/hot_topic_img.png) no-repeat;
 				background-size: 100%;
 				color: #fff;
-				.topic{
+
+				.topic {
 					font-size: 37.5upx;
 					display: inline-block;
 					margin: 16upx;
 				}
-				.watched{
+
+				.watched {
 					font-size: 12px;
 					text-align: right;
-					text{
+
+					text {
 						margin: 0 16upx;
+					}
+				}
+			}
+		}
+	}
+
+	// 铲屎官热议部分
+	.talk {
+		.talk-down {
+			width: 100%;
+			margin-top: 20upx;
+			display: flex;
+			flex-direction: column;
+
+			// border-bottom: 1px solid #f4f4f4;
+			.talk-topic {
+				display: flex;
+				flex-direction: column;
+				border-bottom: 1px solid #f4f4f4;
+
+				.talk-ask {
+					margin: 10upx 20upx;
+					font-size: 16px;
+					height: 42upx;
+					padding: 16upx;
+
+					text {
+						vertical-align: top;
+					}
+				}
+
+				.talk-reply {
+					margin: 0 20upx 20upx 20upx;
+					font-size: 12px;
+					color: #888;
+					background-color: #e8e8e8;
+					border-radius: 16upx;
+					padding: 16upx;
+
+					text {
+						vertical-align: top;
+					}
+				}
+
+				.talk-ask,
+				.talk-reply {
+					.img {
+						display: inline-block;
+						width: 42upx;
+						height: 42upx;
+						margin-right: 10upx;
+					}
+
+					.img.ask {
+						background: url(../../static/icon/wen.png) no-repeat;
+						background-size: 100%;
+					}
+
+					.img.reply {
+						background: url(../../static/icon/da.png) no-repeat;
+						background-size: 100%;
+					}
+				}
+
+				.talk-more {
+					margin: 25upx auto;
+					font-size: 12px;
+					color: #819cff;
+
+					text {
+						vertical-align: top;
+					}
+
+					.next {
+						display: inline-block;
+						width: 20upx;
+						height: 20upx;
+						background: url(../../static/icon/home_hot_icon_more_default.png) no-repeat;
+						background-size: contain;
+						margin-left: 8upx;
+					}
+				}
+			}
+		}
+	}
+
+	// 它嗅故事部分
+	.story {
+		width: 100%;
+		.story-down {
+			.story-article {
+				display: flex;
+				flex-direction: row;
+				width: 90%;
+				margin: 20upx auto;
+				padding: 20upx 0upx;
+				border-bottom: 1px solid #f4f4f4;
+
+				.story-img {
+					width: 225upx;
+					height: 150upx;
+					margin-right: 20upx;
+				}
+
+				.story-content {
+					display: flex;
+					flex-direction: column;
+					justify-content: space-around;
+
+					.title {
+						font-size: 16px;
+					}
+
+					.content {
+						font-size: 12px;
+						color: #888;
+						text-indent: 25upx;
+						overflow: hidden;
+						white-space: wrap;
 					}
 				}
 			}
